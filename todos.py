6242903919fa3fdef08ed2cdb975e7d2e177d089
Koala-Developer <Koala-Developer>
2020-12-17 20:41:24 +0200

    Added TODO-s

--- todos.py
+++ todos.py
@@ -2,9 +2,12 @@
 # TODO: Implement CV Results comparsion with DB plates
 
 # TODO: Let residents edit their registered plates
+# TODO: Limit Plate number actions to owners / admins
 # TODO: Limit plates creation by the resident's limit
 
 
 # TODO: FUTURE -> Implement 'Permits' setting on the UI's register (for easy staff registration)
 # TODO: FUTURE -> Move Permits & Users administration to the UI
 # TODO: FUTURE -> Support expiration date (giving user by rental -> user removed automatically)
+
+# TODO: Remove Duplicate Static dirs (use same for all inner-packages)
